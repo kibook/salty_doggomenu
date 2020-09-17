42ec121fc6a05323e284ebcf5d89ea21cdbae800
kibukj <kibuzhj@gmail.com>
2020-09-17 18:22:22 +0000

    Add more animations

--- client.lua
+++ client.lua
@@ -6,13 +6,21 @@ local bigDogAnimations = {
 	{ dictionary = "creatures@rottweiler@indication@", animation = "indicate_high", name = "Draw Attention", },
 	{ dictionary = "creatures@rottweiler@melee@", animation = "dog_takedown_from_back", name = "Attack", },
 	{ dictionary = "creatures@rottweiler@melee@streamed_taunts@", animation = "taunt_02", name = "Taunt", },
-	{ dictionary = "creatures@rottweiler@swim@", animation = "swim", name = "Swim", },
+	{ dictionary = "creatures@rottweiler@tricks@", animation = "beg_loop", name = "Beg", },
+	{ dictionary = "creatures@rottweiler@tricks@", animation = "paw_right_loop", name = "Shake Paw", },
+	{ dictionary = "creatures@rottweiler@tricks@", animation = "petting_chop", name = "Receiving Pats", },
 }
 
 local smallDogAnimations = {
-	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_a", name = "Itch"},
-	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_b", name = "Sit"},
-	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_c", name = "Lay Down"}
+	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_a", name = "Itch" },
+	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_b", name = "Sit" },
+	{ dictionary = "creatures@pug@amb@world_dog_sitting@idle_a", animation = "idle_c", name = "Lay Down" },
+	{ dictionary = "creatures@pug@amb@world_dog_barking@idle_a", animation = "idle_c", name = "Shake" },
+	{ dictionary = "creatures@pug@amb@world_dog_barking@idle_a", animation = "idle_a", name = "Bark" },
+	{ dictionary = "creatures@pug@move", animation = "idle_turn_0", name = "Dance" },
+	{ dictionary = "creatures@pug@move", animation = "dead_right", name = "Sleep/Death 1" },
+	{ dictionary = "creatures@pug@move", animation = "dead_left", name = "Sleep/Death 2" },
+
 }
 
 local bigDogModels = {
@@ -101,9 +109,9 @@ Citizen.CreateThread(function()
 			WarMenu.OpenMenu('dogmenu')
 		end
 		if emotePlaying then
-            if (IsControlPressed(0, 32) or IsControlPressed(0, 33) or IsControlPressed(0, 34) or IsControlPressed(0, 35)) then
-                cancelEmote()
-            end
-        end
+			if (IsControlPressed(0, 32) or IsControlPressed(0, 33) or IsControlPressed(0, 34) or IsControlPressed(0, 35)) then
+				cancelEmote()
+			end
+		end
 	end
 end)
